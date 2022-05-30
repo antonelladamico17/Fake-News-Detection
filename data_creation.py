01b9b7eb5d757f373838a0a184a00e0c16d71b7f
Antonella D'Amico <60288164+antonelladamico17@users.noreply.github.com>
2022-05-30 17:53:01 +0200

    Create data_creation.py

---
+++ data_creation.py
@@ -0,0 +1,54 @@
+import pickle
+import pandas as pd
+import numpy as np
+import random
+import sys
+import os
+import tldextract
+import warnings
+import regex as re
+from typing import *
+
+
+def parse_url(url: str) -> Optional[Dict[str, str]]:
+    try:
+        no_scheme = not url.startswith('https://') and not url.startswith('http://')
+        if no_scheme:
+            parsed_url = urlparse(f"http://{url}")
+            return {
+                "scheme": None, # not established a value for this
+                "netloc": parsed_url.netloc,
+                "path": parsed_url.path,
+                "params": parsed_url.params,
+                "query": parsed_url.query,
+                "fragment": parsed_url.fragment,
+            }
+        else:
+            parsed_url = urlparse(url)
+            return {
+                "scheme": parsed_url.scheme,
+                "netloc": parsed_url.netloc,
+                "path": parsed_url.path,
+                "params": parsed_url.params,
+                "query": parsed_url.query,
+                "fragment": parsed_url.fragment,
+            }
+    except:
+        return None
+      
+      
+def get_num_subdomains(netloc: str) -> int:
+    subdomain = tldextract.extract(netloc).subdomain 
+    if subdomain == "":
+        return 0
+    return subdomain.count('.') + 1
+  
+  
+  
+tokenizer = RegexpTokenizer(r'[A-Za-z]+')
+def tokenize_domain(netloc: str) -> str:
+    split_domain = tldextract.extract(netloc)
+    no_tld = str(split_domain.subdomain +'.'+ split_domain.domain)
+    return " ".join(map(str,tokenizer.tokenize(no_tld)))
+  
+  
