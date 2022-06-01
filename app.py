e350f2c3fe68bf14af1d206d16090cdf0565a414
Antonella D'Amico <60288164+antonelladamico17@users.noreply.github.com>
2022-06-01 17:39:57 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+def main():
+	st.title("Awesome Streamlit for ML")
+	st.subheader("How to run streamlit from colab")
+
+
+	menu = ["Home","About"]
+	choice = st.sidebar.selectbox('Menu',menu)
+	if choice == 'Home':
+		st.subheader("Streamlit From Colab")	
+
+
+
+if __name__ == '__main__':
+	main()
