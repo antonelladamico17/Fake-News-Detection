910de1d324bfebb7645afd6331ff73df8c521aef
Antonella D'Amico <60288164+antonelladamico17@users.noreply.github.com>
2022-06-01 18:41:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,7 @@ st.set_page_config(page_title="Fake News detector", layout="centered")
 def main():
 	st.title("Fake News Detector")
 	st.write("__________________")
-	st.text("Enter URL")
 
-
-	menu = ["Home","About"]
-	choice = st.sidebar.selectbox('Menu',menu)
-	if choice == 'Home':
-		st.subheader("Streamlit From Colab")	
-		
 	user_input = st.text_input("Enter URL:")
 
 if __name__ == '__main__':
