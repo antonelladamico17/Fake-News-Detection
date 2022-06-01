90393b12ee919c020f665c4b3984f04b87f0eeba
Antonella D'Amico <60288164+antonelladamico17@users.noreply.github.com>
2022-06-01 21:59:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 from unidecode import unidecode
 from nltk.tokenize import word_tokenize
 import re
+from urllib.parse import urlparse
 import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
