290640b8ec84435e1d345b8828a657236178b885
Antonella D'Amico <60288164+antonelladamico17@users.noreply.github.com>
2022-06-02 10:49:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -116,6 +116,12 @@ def get_data(path):
 				  
 	return domain_fake, domain_real, model
 
+class Converter(BaseEstimator, TransformerMixin):
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, data_frame):
+        return data_frame.values.ravel()
 
 def main():
 	st.title("Fake News Detector")
