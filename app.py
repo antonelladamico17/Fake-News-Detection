955b96d33580d0bcf31cbdf13fb1d9fbc7fbbbfd
Antonella D'Amico <60288164+antonelladamico17@users.noreply.github.com>
2022-06-02 12:29:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+ import streamlit as st
 import os
 import pandas as pd
 import tldextract
@@ -151,7 +151,7 @@ def main():
 	submit = st.button('Check')
 	
 			
-	if submit and user_input!="" and (str(user_input).startswith('http://') or str(user_input).stratswith('https://')):
+	if submit and user_input!="" and (user_input.startswith('http://') or user_input.startswith('https://')):
 		soup = BeautifulSoup(urlopen(user_input))
 		
 		df = [user_input]
