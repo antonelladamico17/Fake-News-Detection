344af115c54057dffcabc1dd8f40b1eeba1b00c3
Antonella D'Amico <60288164+antonelladamico17@users.noreply.github.com>
2022-06-02 14:38:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -134,6 +134,9 @@ class Converter(BaseEstimator, TransformerMixin):
 def main():
 	st.title("Fake News Detector")
 	st.write("__________________")
+	st.subheader("Check if the site you are visiting shares fake news.")
+	st.write("1. Paste the URL inside the box.\\2.Click on the check button.\\3.Wait for the result")
+
 	
 	st.sidebar.write('Credits:')
 	st.sidebar.write("""
