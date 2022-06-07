886375c42c3991d7781710296b0f0e7edae446c9
Antonella D'Amico <60288164+antonelladamico17@users.noreply.github.com>
2022-06-07 08:08:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -242,7 +242,7 @@ def main():
 					st.write('The URL domain appears to be registered as a domain that publishes fake news. Please be careful when reading this news, it may contain some false information.')
 				elif df['domain_tokens'][i] in list(domain_real):
 					st.write('Great! This domain is registered as a domain that publishes reliable news. Anyway, always be careful when reading the news online.')
-					image = Image.open('checked.png', width=5)
+					image = Image.open('checked.png', width = 5)
 					st.image(image)
 
 							   
