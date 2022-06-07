ee1c1d6c4ccecbeee805da9d824b24a4d865f590
Antonella D'Amico <60288164+antonelladamico17@users.noreply.github.com>
2022-06-07 08:04:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -243,7 +243,7 @@ def main():
 				elif df['domain_tokens'][i] in list(domain_real):
 					st.write('Great! This domain is registered as a domain that publishes reliable news. Anyway, always be careful when reading the news online.')
 					image = Image.open('checked.png')
-					st.sidebar.image(image)
+					st.image(image)
 
 							   
 				else:
